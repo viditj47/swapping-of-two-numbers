a3f6f73e334e284e538987e947b41697375ee2a9
AayushTyagi1 <42893909+AayushTyagi1@users.noreply.github.com>
2018-10-10 22:56:13 +0530

    Update EXC.C
    we can make program by two variables only so that the space can be save

--- EXC.C
+++ EXC.C
@@ -2,13 +2,13 @@
 #include<conio.h>
 void main()
 {
-int x,y,z;
+int x,y;
 clrscr();
 printf("enter values of x,y;");
 scanf("%d%d",&x,&y);
-z=x;
-x=y;
-y=z;
+x=x+y;
+y=x-y;
+x=x-y;
 printf("A=%d B=%d",x,y);
 getch();
 }
